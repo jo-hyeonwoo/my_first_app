57b277df4da80a51834e11df983095580bfdf984
조현우 <a01058830723@gmail.com>
2025-12-06 23:05:00 +0900

    fix: Remove deprecated kotlinOptions languageVersion configuration
    - Remove deprecated kotlinOptions block from android/build.gradle.kts
    - Kotlin plugin 2.2.20 already supports latest Kotlin version
    - Fixes deprecated error in build process

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,18 +5,6 @@ allprojects {
     }
 }
 
-subprojects {
-    afterEvaluate {
-        if (plugins.hasPlugin("org.jetbrains.kotlin.android") || plugins.hasPlugin("kotlin-android")) {
-            tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-                kotlinOptions {
-                    languageVersion = "1.9"
-                    apiVersion = "1.9"
-                }
-            }
-        }
-    }
-}
 
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
